a48debbffd291fd638073aba40a4a095df97dafb
Mateusz Mączyński <mateuszmaczynski@gmail.com>
2018-10-20 12:46:12 +0200

    Moved previuos code

---
+++ src/components/devmeeting.vue
@@ -0,0 +1,50 @@
+<template>
+    <div class="devmeeting" id="app">
+        <ul>
+            <li v-for="item in shoppingList" :key="item.id">
+                {{ item.name }}
+            </li>
+        </ul>
+        <button v-on:click="addProduct()">Dodaj</button>
+        <input v-model="name" placeholder="name"/>
+    </div>
+</template>
+
+<script>
+    export default {
+        name: 'devmeeting',
+        data() {
+            return {
+                shoppingList: [
+                    {
+                        id: 1,
+                        name: 'bread'
+                    },
+                    {
+                        id: 2,
+                        name: 'cheese'
+                    },
+                    {
+                        id: 3,
+                        name: 'mineral water'
+                    }
+                ],
+                name: ""
+            }
+        },
+        props: {
+            msg: String,
+        },
+        methods: {
+            addProduct() {
+                let index = this.shoppingList[this.shoppingList.length] + 1;
+                this.shoppingList.push({
+                    id: index,
+                    name: this.name
+                })
+            }
+        }
+    };
+</script>
+
+<style scoped></style>
